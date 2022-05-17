1a8a6126b88d8e6220e097d6b80487ec6c79a7a2
Ahrorbek Mirzaev <ahror@epos.uz>
2022-05-18 01:09:51 +0500

    add NFT to contract and compiling options

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+ 
+const { ALCHEMY_KEY_RINKEBY, ALCHEMY_KEY_MAINNET, ACCOUNT_PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.3",
+  defaultNetwork: "ethereum",
+  networks: {
+    hardhat: {},
+      rinkeby: {
+        url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY_RINKEBY}`,
+        accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+      },
+      ethereum: {
+        chainId: 1,
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY_MAINNET}`,
+        accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+      },
+  },
 };
